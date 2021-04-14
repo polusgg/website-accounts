5182e80c3b095441e9773b756fc3d6cb50638ba5
Cody Scott <cs475x@icloud.com>
2021-04-14 00:50:00 -0700

    Trust load balancer proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,9 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies;
+    protected $proxies = [
+        '10.108.0.4',
+    ];
 
     /**
      * The headers that should be used to detect proxies.
