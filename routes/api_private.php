3c454d0490adbc8cbd4b745a576d7a431e3d4055
Cody Scott <cs475x@icloud.com>
2021-04-16 19:14:54 -0700

    Remove the aforementioned function

--- routes/api_private.php
+++ routes/api_private.php
@@ -4,17 +4,12 @@ use App\Http\Controllers\PrivateApiController;
 use App\Http\Resources\ErrorResource;
 use Illuminate\Support\Facades\Route;
 
-function sendUserNotFoundResponse()
-{
-    return response()->json(new ErrorResource('User not found'), 404);
-}
-
 Route::middleware('auth:api-private')->prefix('v1')->group(function () {
     Route::prefix('users')->group(function () {
         Route::get('{user}', [PrivateApiController::class, 'getUser'])
-            ->missing('sendUserNotFoundResponse');
+            ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
         Route::post('{user:discord_snowflake}/roles', [PrivateApiController::class, 'updateDiscordRoles'])
-            ->missing('sendUserNotFoundResponse');
+            ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
     });
 
     Route::prefix('logs')->group(function () {
