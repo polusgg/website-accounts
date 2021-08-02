afab5eaf563c7f4842229fba52170c27f3da8d80
Cody Scott <cs475x@icloud.com>
2021-08-01 17:35:15 -0700

    Add temporary route to migrate Discord role refresh logic to a scoped route

--- routes/api_private.php
+++ routes/api_private.php
@@ -12,6 +12,8 @@ Route::middleware('auth:api-private')->prefix('v1')->group(function () {
             ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
         Route::post('{user:discord_snowflake}/roles', [PrivateApiController::class, 'updateDiscordRoles'])
             ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
+        Route::post('discord/{user:discord_snowflake}/roles', [PrivateApiController::class, 'updateDiscordRoles'])
+            ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
         Route::put('{user}/options', [PrivateApiController::class, 'updateGameConfig'])
             ->missing(fn() => response()->json(new ErrorResource('User not found'), 404));
     });
